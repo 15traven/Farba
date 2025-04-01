4fa70e3489e46bf071c08a6c2ab7e1d0f8aa14ef
Mykyta <sikachyna.n@gmail.com>
2025-04-01 16:23:01 +0300

    feat: configure window

--- Farba/MainWindow.xaml.cs
+++ Farba/MainWindow.xaml.cs
@@ -13,24 +13,16 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Farba
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-        }
+            this.ExtendsContentIntoTitleBar = true;
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
+            AppWindow.Resize(new Windows.Graphics.SizeInt32(400, 600));
         }
     }
 }
